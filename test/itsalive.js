18d68584e585c45ef271d3997464237b90870c92
Zohaib Rahman <zohaibr@gmail.com>
2017-01-24 15:49:09 -0500

    pushing over

--- test/itsalive.js
+++ test/itsalive.js
@@ -4,6 +4,7 @@ const expect = chai.expect;
 const spies = require('chai-spies');
 chai.use(spies);
 const model = require('../models');
+const Page = model.Page;
 
 describe('first test', function() {
   it('basic arithmetic', function() {
@@ -29,15 +30,63 @@ describe('first test', function() {
 });
 
 describe('Page model', function() {
-  describe('attributes', function() {
-    it('has 5 properties', function() {
-      model.Page
+  describe('Virtuals', function () {
+    var page;
+    beforeEach(function(done) {
+      page = Page.build();
+      done();
     });
-    it('has title property', function() {});
-    it('has urlTitle property', function() {});
-    it('has content property', function() {});
-    it('has status property', function() {});
-    it('has tags property', function() {});
+    describe('route', function () {
+      it('returns the url_name prepended by "/wiki/"', function() {
+          page.urlTitle = 'Testing_the_route';
+          expect(page.route).to.equal('/wiki/Testing_the_route');
+      });
+    });
+    describe('renderedContent', function () {
+      it('converts the markdown-formatted content into HTML', function() {
+        page.content = 'Wooh this is annoying';
+        expect(page.renderedContent).to.equal('<p>Wooh this is annoying</p>\n');
+      });
+    });
+  })
+
+  describe('Class methods', function () {
+    var page;
+    beforeEach(function(done) {
+      page = Page.create({
+        title: 'Testing the class method',
+        content: 'Wooh this is still annoying',
+        tags: ['annoying', 'thisisstillannoying']
+      })
+      .then(function() {
+        done();
+      })
+      .catch(done);
+    })
+
+    describe('findByTag', function () {
+      it('gets pages with the search tag', function() {
+
+      });
+      it('does not get pages without the search tag',function() {});
+    });
   });
-  describe('route', function() {});
+
+  describe('Instance methods', function () {
+    describe('findSimilar', function () {
+      it('never gets itself');
+      it('gets other pages with any common tags');
+      it('does not get other pages without any common tags');
+    });
+  });
+
+  describe('Validations', function () {
+    it('errors without title');
+    it('errors without content');
+    it('errors given an invalid status');
+  });
+
+  describe('Hooks', function () {
+    it('it sets urlTitle based on title before validating');
+  });
 });
